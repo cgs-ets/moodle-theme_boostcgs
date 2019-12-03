4e7f9909f0f18f5f8408c799d3e6de9691ef2fc7
Michael Vangelovski <michael.vangelovski@hotmail.com>
2019-12-03 16:51:46 +1100

    Add turn editing on toggle.

---
+++ classes/output/core_renderer.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace theme_boostcgs\output;
+
+use moodle_url;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Renderers to align Moodle's HTML with that expected by Bootstrap
+ *
+ * @package    theme_boostcgs
+ * @copyright  2012 Bas Brands, www.basbrands.nl
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class core_renderer extends \core_renderer {
+
+    /**
+     * Returns HTML to display a "Turn editing on/off" button in a form.
+     *
+     * @param moodle_url $url The URL + params to send through when clicking the button
+     * @return string HTML the button
+     */
+    public function edit_button(moodle_url $url) {
+        $url->param('sesskey', sesskey());
+        $class = 'btn-turnediting btn-turneditingoff';
+        if ($this->page->user_is_editing()) {
+            $url->param('edit', 'off');
+            $editstring = get_string('turneditingoff');
+        } else {
+        	$class = 'btn-turnediting btn-turneditingon';
+            $url->param('edit', 'on');
+            $editstring = get_string('turneditingon');
+        }
+        return $this->single_button($url, $editstring, 'post', ['class' => $class]);
+    }
+}
