b04ffa1908ec55926830d3d90d8afa0023e2242c
Michael Vangelovski <michael.vangelovski@hotmail.com>
2021-02-08 11:46:28 +1100

    Add self enrol button to header.

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -112,8 +112,11 @@ class core_renderer extends \core_renderer {
         $header->contextheader = $this->context_header();
         $header->hasnavbar = empty($PAGE->layout_options['nonavbar']);
         $header->navbar = $this->navbar();
-        $header->pageheadingbutton = $this->page_heading_button();
         $header->courseheader = $this->course_header();
+        $header->pageheadingbutton = $this->page_heading_button();
+        $header->selfenrolbutton = $this->self_enrol_button();
+
+        //var_export($header->selfenrolbutton); exit;
 
         $profileuser = '';
         if ($PAGE->pagetype === "course-view-tiles") {
@@ -132,6 +135,36 @@ class core_renderer extends \core_renderer {
         }
 
         return $this->render_from_template('theme_boost/full_header', $header);
+    }
+
+    protected function self_enrol_button() {
+        global $PAGE;
+
+        $course = $PAGE->course;
+        $coursecontext = \context_course::instance($course->id);
+        $instances = enrol_get_instances($course->id, true);
+        $plugins   = enrol_get_plugins(true);
+
+        $button = '';
+        if ($course->id == SITEID or isguestuser() or !isloggedin() or is_enrolled($coursecontext) or is_viewing($coursecontext)) {
+            // Do not show self enrol button.
+        } else {
+            foreach ($instances as $instance) {
+                if (!isset($plugins[$instance->enrol])) {
+                    continue;
+                }
+                $plugin = $plugins[$instance->enrol];
+                if ($plugin->show_enrolme_link($instance)) {
+                    $url = new moodle_url('/enrol/index.php', array('id'=>$course->id));
+                    $shortname = format_string($course->shortname, true, array('context' => $coursecontext));
+                    $title = get_string('enrolme', 'core_enrol', $shortname);
+                    //$button = $this->single_button($url, $title);
+                    $button = '<a class="btn btn-secondary btn-self-enrol" href="' . $url . '">' . $title . '</a>';
+                    break;
+                }
+            }
+        }
+        return $button;
     }
 
 }
